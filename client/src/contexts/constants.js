7573f89db36ad95b700a6355397e9807b6d0e79f
Quang Huy <quanghuy.h20@gmail.com>
2021-07-15 17:36:19 +0700

    Done

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,3 +1,3 @@
-export const apiURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'deployedURL'
+export const apiURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://evening-wave-30905.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'userInfo'
